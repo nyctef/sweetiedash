1948fa520671444fc7cf78b169fd81da4fc1522a
Nyctef <nyctef@nyctef.com>
2013-12-15 20:48:02 +0000

    initial commit

---
+++ sweetiedash/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sweetiedash.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var jsonResponse = await new HttpClient().GetAsync("http://nyc-02.cloudapp.net");
+            var json = await jsonResponse.Content.ReadAsStringAsync();
+
+            ViewBag.Model = JObject.Parse(json);
+            return View();
+        }
+    }
+}
