9206854b70950464ef953fbe955ca5739b9e4061
Nyctef <nyctef@nyctef.com>
2014-04-13 01:54:08 +0100

    formatting, first bar chart

--- sweetiedash/Scripts/charts.js
+++ sweetiedash/Scripts/charts.js
@@ -1,1 +1,69 @@
-﻿
+﻿"use strict";
+
+// based on http://bl.ocks.org/mbostock/3885304
+function barChart(parentSelector, dict) {
+
+    var data = [];
+    for (var key in dict) {
+        data.push({ key: key, value: +dict[key] });
+    }
+    console.log(data);
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var svg = d3.select(parentSelector).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(data.map(function (d) { return d.key; }));
+    var ymax = d3.max(data, function (d) { return d.value; });
+    console.log('ymax:'+ymax);
+    y.domain([0, ymax]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end");
+        //.text("Frequency");
+
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.key); })
+        .attr("width", x.rangeBand())
+        .attr("y", function (d) { return y(d.value); })
+        .attr("height", function (d) { return height - y(d.value); });;
+
+    function type(d) {
+        d.value = +d.value;
+        return d;
+    }
+
+}
